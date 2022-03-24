7ff7a5b039950158f802feb504038a8b1d16422d
unknown <nilmanikulaweera@gmail.com>
2022-03-25 02:45:40 +0530

    done audio file upload functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { FileService } from './file.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,25 +6,7 @@ import { FileService } from './file.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sentence: string = "";
-  selectedFile: string = "";
-  status: string = "";
 
-  constructor(private fileService: FileService) {}
-
-  onUploadFiles(files: File[]): void {
-    const formData = new FormData();
-    for (const file of files) {
-      this.selectedFile = file.name;
-      formData.append('file', file, file.name);
-    }
-    this.status = "processing";
-    this.fileService.upload(formData)
-      .subscribe(
-        sentence => {
-          this.status = "finished";
-          this.sentence = sentence!;
-        }
-      );
-  }
+  constructor() {}
+  
 }
